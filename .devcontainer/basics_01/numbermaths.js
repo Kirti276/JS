3fda8094c1eb853c15b990759f8bb5e2cfd0f4db
Kirti276 <thisiskirti28@gmail.com>
2025-01-03 06:42:59 +0000

    saved file

---
+++ .devcontainer/basics_01/numbermaths.js
@@ -0,0 +1,20 @@
+const s=400
+console.log(s)
+const s1=new Number(400)
+console.log(s1)
+console.log(s1.toString())
+console.log(s1.toFixed(2))//adds precision
+
+const num=10000000
+console.log(num.toLocaleString('en-IN'))
+
+// MATH
+// max,min,pi,ceil,floor,round,abs
+console.log(Math.random()) // value between 0 and 1 including both
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10+1))
+
+min=10
+max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
---
+++ .devcontainer/basics_01/strings.js
@@ -0,0 +1,15 @@
+console.log("hello"+"world")
+let name ="kirti"
+let age=18
+console.log(`hello my nmae is ${name} and my age is ${age}`) //use of backticks 
+
+let n=new String("mango")//method to declare string as object
+console.log(n[1])//object as key value pair so at 1 index a is there
+console.log(n.length)
+console.log(n.toUpperCase())
+console.log(n.charAt(3))
+console.log(n.indexOf('o'))
+const newstring=n.substring(0,4)//cannot take negative values
+console.log(newstring)
+const anotherstring=n.slice(0,2)
+console.log(anotherstring)
